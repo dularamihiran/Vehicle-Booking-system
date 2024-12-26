5561037068cffb95b726ed6600793146a4e23132
dularamihiran <168359371+dularamihiran@users.noreply.github.com>
2024-12-26 19:06:27 +0530

    change controlle

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
+    // Main Dashboard (Default View)
     public function index()
     {
         // Fetch all vehicles
@@ -18,5 +19,25 @@ class DashboardController extends Controller
 
         // Pass data to the dashboard view
         return view('dashboard', compact('vehicles', 'userBookedVehicles'));
+    }
+
+    // Show Available Vehicles
+    public function availableVehicles()
+    {
+        // Fetch all available vehicles
+        $vehicles = Vehicle::all();
+
+        // Pass data to the available vehicles view
+        return view('dashboard.available', compact('vehicles'));
+    }
+
+    // Show Booked Vehicles
+    public function bookedVehicles()
+    {
+        // Fetch the logged-in user's booked vehicles
+        $userBookedVehicles = BookedVehicle::where('user_id', Auth::id())->get();
+
+        // Pass data to the booked vehicles view
+        return view('dashboard.booked', compact('userBookedVehicles'));
     }
 }
